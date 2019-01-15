9a4099c32d14ad1e027aa6241c68cf32552caac8
Brendan McCray <rbmccr@gmail.com>
2019-01-15 14:56:01 -0600

    finished exercise and some extra practice

--- queries.py
+++ queries.py
@@ -22,25 +22,81 @@
   # SELECT a.Title, s.Title FROM Album a LEFT JOIN Song s ON s.AlbumId = a.AlbumId;
   # SELECT a.Title, s.Title FROM Song s LEFT JOIN Album a ON s.AlbumId = a.AlbumId;
 
-# Answer: SELECT a.Title as 'Album Title', s.Title as 'Song Title', art.ArtistName as 'Artist Name'
+# Answer (no NULL values returned):
+#         SELECT Song.Title as 'Song Title', Album.Title as 'Album Title', Ar       tist.ArtistName as 'Artist Name'
+#         FROM Song
+#         LEFT JOIN Album ON Album.AlbumId = Song.AlbumId
+#         LEFT JOIN Artist ON Album.artistID = Artist.artistID
+#         WHERE Album.Title = "Absolution" and Artist.ArtistName = "Muse"
+#
+# Answer 2 (can return NULL values)
+# SELECT a.Title as 'Album Title', s.Title as 'Song Title', art.ArtistName as 'Artist Name'
 #         FROM Album a
 #         LEFT JOIN Artist art ON a.artistID = art.artistID
 #         LEFT JOIN Song s ON s.AlbumId = a.AlbumId
 #         WHERE a.Title = "Absolution" and art.ArtistName = "Muse"
-# Note: a regular join works here, too
 
 # 6. Write a SELECT statement to display how many songs exist for each album. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
-# Answer: SELECT a.Title as 'Album title', count() as 'Total Songs'
-#         FROM Song s, Album a
-#         WHERE s.albumID = a.albumID
-#         GROUP BY a.Title
+# Answer: SELECT Album.Title, count() as "Total Songs"
+#         FROM Album
+#         JOIN Song on Song.AlbumId = Album.AlbumId
+#         GROUP BY Song.AlbumId
 
 # 7. Write a SELECT statement to display how many songs exist for each artist. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+# Answer: SELECT Artist.ArtistName as "Artist", count(*) as "Number of Songs"
+#         From Song
+#         LEFT JOIN Artist ON Song.ArtistId = Artist.ArtistId
+#         GROUP BY Song.ArtistId
+
+# Answer 2: SELECT Artist.ArtistName as "Artist", count(*) as "Number of Songs"
+#           From Artist
+#           JOIN Song ON Song.ArtistId = Artist.ArtistId
+#           GROUP BY Song.ArtistId
+
+# Answer 3 (shows all artists, even artists with 0 songs):
+#           SELECT Artist.ArtistName as "Artist", count(Song.ArtistId) as "Number of Songs"
+#           From Artist
+#           LEFT JOIN Song ON Song.ArtistId = Artist.ArtistId
+#           GROUP BY Artist.ArtistId
 
 # 8. Write a SELECT statement to display how many songs exist for each genre. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+# Answer: SELECT Genre.Label as "Genre", count(Song.GenreId)
+#         FROM Song
+#         LEFT JOIN Genre ON Song.GenreId = Genre.GenreId
+#         GROUP BY Song.GenreId
 
 # 9. Using MAX() function, write a select statement to find the album with the longest duration. The result should display the album title and the duration.
+# Answer: SELECT Album.Title, MAX(Album.AlbumLength) as "Album Duration"
+#         FROM Album
 
 # 10. Using MAX() function, write a select statement to find the song with the longest duration. The result should display the song title and the duration.
+# Answer: SELECT Song.Title, MAX(Song.SongLength) as "Song Duration"
+#         FROM Song
 
 # 11. Modify the previous query to also display the title of the album.
+# Answer: SELECT Song.Title, MAX(Song.SongLength) as "Song Duration", Album.Title as "Album Title"
+#         FROM Song
+#         LEFT JOIN Album
+
+# EXTRA PRACTICE --------------------------------------------------------
+
+# Get Number of albums by each artist
+# Answer: SELECT Artist.ArtistName, count() as "Number of Albums"
+#         FROM Album
+#         LEFT JOIN Artist ON Artist.ArtistId = Album.ArtistId
+#         GROUP BY Album.ArtistId
+
+# Get songs that are classified as rock, pop, or punk
+# Note: genreId is not mandatory (may be null) on song
+# Answer: SELECT Song.Title as "Song Name", Genre.Label as "Genre"
+#         FROM Song
+#         JOIN Genre ON Genre.GenreId = Song.GenreId
+#         WHERE Genre.GenreId = 2 OR Genre.GenreId = 7 OR Genre.GenreId = 9
+
+# Get the TOTAL NUMBER songs that are classified as rock, pop, or punk
+# Group by Genre
+# Answer: SELECT Genre.Label as "Genre", count(*) as "Total Songs"
+#         FROM Genre
+#         JOIN Song ON Song.GenreId = Genre.GenreId
+#         WHERE Song.GenreId = 2 OR Song.GenreId = 7 OR Song.GenreId = 9
+#         GROUP BY Song.GenreId
